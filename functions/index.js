1d604cdd225d5412f62fa46c9262fec20926898a
Weliton Sousa <weliton@pop-os.localdomain>
2021-03-05 22:54:04 -0300

    backend do perfil/blog concluido

--- functions/index.js
+++ functions/index.js
@@ -2,19 +2,19 @@ const functions = require("firebase-functions");
 const admin = require("firebase-admin");
 const firebaseKey = require("./firebase-key.json");
 const express = require("express");
-const app = express();
 const profile = require("./services/profile");
-
 const {getAllPosts, getPost, postNewPost} = require("./services/posts");
+const cors = require("cors");
+const app = express();
 
+app.use(cors());
 admin.initializeApp(admin.credential.cert(firebaseKey));
-
 // routes of profile
 app.get("/profile", profile);
 
 // routes activities of posts
 app.get("/activities/posts", getAllPosts);
-app.get("/activities/post", getPost);
+app.get("/activities/post:id", getPost);
 app.post("/activities/post", postNewPost);
 
 exports.api = functions.https.onRequest(app);
--- functions/services/posts.js
+++ functions/services/posts.js
@@ -5,13 +5,20 @@ const error = (res, statusError, response) => {
   return res.status(statusError).json(response);
 };
 
+const dateToYMD = (date) => {
+  const d = date.getDate();
+  const m = date.getMonth() + 1;
+  const y = date.getFullYear();
+  return (d <= 9 ? "0" + d : d)+ "/" + (m<=9 ? "0" + m : m) + "/" + "" + y;
+  // return "" + y + "/" + (m<=9 ? "0" + m : m) + "/" + (d <= 9 ? "0" + d : d);
+};
+
 const getAllPosts = async (req, res) => {
   try {
     const data = await admin.firestore().collection("posts").get();
     const response = data.docs.map( (doc) => {
       return {id: doc.id, data: doc.data()};
     });
-
     res.send({success: true, data: response});
   } catch (e) {
     error(res, 501, {success: false, message: "Algo deu errado"});
@@ -19,10 +26,9 @@ const getAllPosts = async (req, res) => {
 };
 
 const getPost = async (req, res) => {
+  const id = req.params.id;
   try {
-    const id = req.body.id;
     const data = await admin.firestore().collection("posts").doc(id).get();
-    console.log(data.data());
 
     res.send({success: true, data: data.data() ? data.data() : []});
   } catch (e) {
@@ -31,24 +37,28 @@ const getPost = async (req, res) => {
 };
 
 const postNewPost = async (req, res) => {
-  const {title, smallDescription, fullDescription, link, image} = req.body;
+  const {title, smallDescription, fullDescription, repo, image, imageExample} =
+   req.body;
   const token = `${req.headers.token}`;
 
   try {
     if (title && smallDescription && fullDescription &&
-        link !== undefined && image !== undefined && auth.token === token) {
+      image !== undefined && auth.token === token) {
       await admin.firestore().collection("posts").add({
         title: title,
         smallDescription: smallDescription,
         fullDescription: fullDescription,
         image: image,
-        link: link,
+        repo: repo.title && repo.link ? repo : {},
+        imageExample: imageExample,
+        date: `${dateToYMD(new Date())}`,
       });
       res.send({success: true, message: "Post adicionado"});
     } else {
       error(res, 401, {success: false, message: "Não autorizadoo"});
     }
   } catch (e) {
+    console.log(e);
     error(res, 501, {success: false, message: "Algo deu errado"});
   }
 };
